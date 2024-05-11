2e7317e61d3e9977a68e0ba781de209506b94e22
Harshit Sharma <144817449+Harshitsharmax@users.noreply.github.com>
2024-05-11 14:54:51 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # Loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+diabetes_model = pickle.load(open('C:/Users/Harshit sharma/Desktop/Multiple_disease_prediction_system/diabetes_model.sav','rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('C:/Users/Harshit sharma/Desktop/Multiple_disease_prediction_system/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
+parkinsons_model = pickle.load(open('C:/Users/Harshit sharma/Desktop/Multiple_disease_prediction_system/parkinsons_model.sav','rb'))
 
 
 
